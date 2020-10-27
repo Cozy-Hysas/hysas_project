0ce8f59ee53c2efce7589fccee71cce51ce3ac16
sami affes <sami_affes@yahoo.com>
2020-10-27 14:34:48 +0100

    create component login and create file validation for the login

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,4 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
-ReactDOM.render(<h1>hello world</h1>,document.getElementById("root"))
+import Login from "./components/Login.jsx"
+ReactDOM.render(<Login/>,document.getElementById("root"))
