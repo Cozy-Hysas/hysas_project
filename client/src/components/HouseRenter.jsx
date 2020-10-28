b71da31ebc640a31798c54274e58be2e6759367c
helayounsi <hela.ahmedyounsi@gmail.com>
2020-10-28 05:12:35 +0100

    added homePage footer and header

--- client/src/components/HouseRenter.jsx
+++ client/src/components/HouseRenter.jsx
@@ -29,7 +29,7 @@ class HouseRenter extends React.Component {
         e.preventDefault()
         var newHouse = {title: this.state.title, imageUrl: this.state.imageUrl, description: this.state.description, adress: this.state.adress, price: this.state.price, startDate: this.state.startDate, endDate: this.state.endDate};
         console.log(newHouse);
-        $.post('/House/addHouse', newHouse, (err,results) => {
+        $.post('/House/addHouse', newHouse, (err, results) => {
             if(err) console.log(err);
             else {
                 console.log(results);
@@ -42,7 +42,7 @@ class HouseRenter extends React.Component {
         return (
             <div>
             <div className="add">
-                <div className="add-bike">
+                <div className="add-house">
                     <h3>Enter information for the House to rent</h3>
             <form onSubmit={this.handleEventOnClick}>
                 Title: <input className="input" type="text" name="title" onChange={this.handleEventOnChange} required/>
@@ -50,8 +50,8 @@ class HouseRenter extends React.Component {
                 Description: <textarea className="textarea" name="description" cols="30" rows="10" onChange={this.handleEventOnChange}required></textarea>
                 Adress: <input className="input" type="text" name="adress" onChange={this.handleEventOnChange} required/>
                 Price: <input className="input" type="number" name="price" min ='0' max = '100' onChange={this.handleEventOnChange}required/>
-                daysOfService:  startDate <input className="input" type='date' name="startDate" onChange={this.handleEventOnChange}required/>
-                                EndDate <input className="input" type="date" name="endDate" onChange={this.handleEventOnChange}required/>
+                daysOfService:   startDate <input className="input" type='date' name="startDate" onChange={this.handleEventOnChange}required/>
+                                 EndDate <input className="input" type="date" name="endDate" onChange={this.handleEventOnChange}required/>
                 
                 <button className="btn btn-success" type="submit" >Add my House</button>
             </form>
